528974bead2b16ec9a1d9380ba381857c4554e9d
Bryan Personal <bryanlohjw97@gmail.com>
2023-07-29 12:52:11 +0800

    init

---
+++ spotify-remote-queue/src/main/java/com/bryan/spotifyremotequeue/SpotifyRemoteQueueApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.spotifyremotequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRemoteQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyRemoteQueueApplication.class, args);
+	}
+
+}
---
+++ spotify-remote-queue/src/test/java/com/bryan/spotifyremotequeue/SpotifyRemoteQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.spotifyremotequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyRemoteQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
