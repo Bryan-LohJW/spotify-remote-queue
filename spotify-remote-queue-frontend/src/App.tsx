2b0eb29ee1d5ecbac7ad31208bc7f8ae520af002
Bryan Personal <bryanlohjw97@gmail.com>
2023-08-19 12:38:00 +0800

    nit: features to test endpoints

--- spotify-remote-queue-frontend/src/App.tsx
+++ spotify-remote-queue-frontend/src/App.tsx
@@ -1,15 +1,27 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home/Home';
+import SpotifyResponse from './pages/spotify-response/SpotifyResponse';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const router = createBrowserRouter([
 	{
 		element: <Home />,
 		path: '/',
 	},
+	{
+		element: <SpotifyResponse />,
+		path: '/spotify-response',
+	},
 ]);
 
+const queryClient = new QueryClient();
+
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 };
 
 export default App;
